e649c94308e90602e8d9930a3b95ef4aad4448ce
Renzo Martin Poggio <63516530+Renzo904@users.noreply.github.com>
2024-12-11 19:18:44 -0300

    First mod Implementation(WIP)

---
+++ README.md
@@ -0,0 +1,19 @@
+# TekaTeka
+ Custom Song Loader for Taiko no Tatsujin: Rhythm Festival (WIP)
+ 
+# Requirements
+ Visual Studio 2022 or newer\
+ Taiko no Tatsujin: Rhythm Festival
+ 
+ 
+# How to use
+ TODO
+
+# Build
+ Install [BepInEx be 697](https://builds.bepinex.dev/projects/bepinex_be) or [BepInEx 6.0.0-pre.2](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) into your Rhythm Festival directory and launch the game.\
+ This will generate all the dummy dlls in the interop folder that will be used as references.\
+ Make sure you install the Unity.IL2CPP-win-x64 version.\
+ Newer versions of BepInEx could have breaking API changes until the first stable v6 release, so those are not recommended at this time.
+ 
+ Attempt to build the project, or copy the .csproj.user file from the Resources file to the same directory as the .csproj file.\
+ Edit the .csproj.user file and place your Rhythm Festival file location in the "GameDir" variable.
---
+++ TekaTeka/Plugins/CustomSongLoader.cs
@@ -0,0 +1,92 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekaTeka.Plugins
+{
+    internal class CustomSongLoader
+    {
+        const string CHARTS_FOLDER = "fumen";
+        const string PRACTICE_DIVISIONS_FOLDER = "fumencsv";
+
+
+        static readonly string songsPath = Path.Combine(BepInEx.Paths.GameRootPath, "TekaSongs");
+
+
+        #region Append Custom Songs DB
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MusicDataInterface), "LoadDataFromFile")]
+        static void LoadSongsDatabase_Postfix(MusicDataInterface __instance, ref string path)
+        {
+            
+            var bytes = Cryptgraphy.ReadAllAesAndGZipBytes(path, Cryptgraphy.AesKeyType.Type2);
+            string jsonString = Encoding.UTF8.GetString(bytes);
+            var musicInfolist = JsonSupports.ReadJson<MusicDataInterface.MusicInfo>(jsonString);
+
+
+            for (int i = 0; i < musicInfolist.Count; i++)
+            {
+                MusicDataInterface.MusicInfo song = musicInfolist[i];
+
+                __instance.AddMusicInfo(ref song);
+            }
+        }
+
+        #endregion
+
+        #region Load Custom Chart
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FumenLoader.PlayerData), "ReadCoroutine")]
+        static void ReadSongChart_Prefix(FumenLoader.PlayerData __instance, ref string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return;
+            };
+            string fileName = Path.GetFileName(filePath);
+            filePath = Path.Combine(songsPath, CHARTS_FOLDER, fileName);
+
+        }
+
+        #endregion
+
+        #region Load Custom Practice Chart
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FumenDivisionManager), nameof(FumenDivisionManager.Load))]
+        static bool ReadSongPracticeDivision_Prefix(FumenDivisionManager __instance, ref string musicuid)
+        {
+
+            if (File.Exists(Path.Combine(UnityEngine.Application.streamingAssetsPath, PRACTICE_DIVISIONS_FOLDER, musicuid + ".bin")))
+            {
+                return true;
+            };
+            string filePath = Path.Combine(songsPath, PRACTICE_DIVISIONS_FOLDER, musicuid + ".bin");
+
+
+            var bytes = Cryptgraphy.ReadAllAesAndGZipBytes(filePath, Cryptgraphy.AesKeyType.Type2);
+            string csvString = Encoding.UTF8.GetString(bytes);
+            var datas = __instance.loader_.CreateDivisionData(csvString);
+            __instance.datas_ = datas;
+            __instance.IsLoadFinished = true;
+            __instance.musicuId_ = musicuid;
+            return false;
+        }
+
+        #endregion
+
+        #region Load Custom Song file
+
+        // TODO
+
+        #endregion
+
+
+
+    }
+}
